88ab8713f51b166c0269ad7ed60559d5a84a9591
Ambre-guia <ambreguia77@gmail.com>
2024-10-25 14:01:57 +0200

    articles update

--- account/forms.py
+++ account/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from django.contrib.auth.models import User
+from .models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email',  'password1', 'password2')
+        fields = ( 'email', 'last_name', 'first_name',  'password1', 'password2')
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('email','last_name', 'first_name', 'password')
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Articles, Categories
 
 # Create your views here.
+def article_list(request):
+
+    categories = Categories.objects.all()
+    categories_id = request.GET.get('categories_id')
+    if categories_id:
+        articles = Articles.objects.filter(categories_id=categories_id)
+    else:
+        articles = Articles.objects.all()
+        
+    is_empty = not articles.exists()
+    context = {
+            'articles': articles,
+            'categories': categories,
+            'is_empty': is_empty,  
+        }
+    return render(request, 'shop/article_list.html', context)
+
+def article_detail(request, article_id):
+    return render(request, 'shop/article_detail.html', {'article_id': article_id})
