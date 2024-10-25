176c3c127227dce64782b183fa46d01603081557
Ambre-guia <ambreguia77@gmail.com>
2024-10-25 14:16:56 +0200

    admin update

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 
+from .models import Articles, Categories
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'description', 'prix', 'stock', 'categorie_id')
+    list_filter = ('categorie_id',)    
+    search_fields = ('nom', 'categorie_id')
+
+admin.site.register(Articles, ArticleAdmin)
+
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('nom',)    
+    search_fields = ('nom',)
+
+admin.site.register(Categories, CategorieAdmin)
